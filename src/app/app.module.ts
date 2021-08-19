eadf46696651ea2c655c9c0563bfcbd4933ff0be
jimena <jimenahernando@hotmail.com>
2021-08-19 15:00:29 +0200

    Componentes:form, listaTareas y filtros, los agrego al module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { FiltrosComponent } from './components/filtros/filtros.component';
+import { ListatareasComponent } from './components/listatareas/listatareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FiltrosComponent,
+    ListatareasComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/listatareas/listatareas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listatareas',
+  templateUrl: './listatareas.component.html',
+  styleUrls: ['./listatareas.component.css']
+})
+export class ListatareasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
